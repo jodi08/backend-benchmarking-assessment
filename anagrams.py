63e5e17b660e8b18adce70e9b21217b75e140e23
Jo Anna Mollman <joanna.mollman@gmail.com>
2020-06-27 11:07:19 -0700

    finish project

--- anagrams.py
+++ anagrams.py
@@ -10,14 +10,20 @@ for an arbitrary list of strings.
 
 # Your name here, and any other people/sources who helped.
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = "Jo Anna Mollman (jodi08 on github)"
 
 import sys
+import pdb
+import cProfile
+import pstats
+import functools
 
 
 def alphabetize(string):
     """Returns alphabetized version of the string."""
+    print(string)
     return "".join(sorted(string.lower()))
+    
 
 
 def find_anagrams(words):
@@ -27,11 +33,12 @@ def find_anagrams(words):
     Example:
     {'dgo': ['dog'], 'act': ['cat', 'act']}
     """
-    anagrams = {
-        alphabetize(word): [
-            w for w in words
-            if alphabetize(w) == alphabetize(word)]
-        for word in words}
+    anagrams = {}
+    for word in words:
+        if ''.join(sorted(word)) in anagrams:
+            anagrams[''.join(sorted(word))].append(word)
+        else:
+            anagrams[''.join(sorted(word))] = [word]
     return anagrams
 
 
